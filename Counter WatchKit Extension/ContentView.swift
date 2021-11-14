34255ab7cd4ac25d057997c637fc41355322d5ec
Lookforrachel <lookfor.rachel@gmail.com>
2021-11-13 22:00:07 -0600

    Not sure how to make the reset button bigger without having weird colour blocks in it

--- Counter WatchKit Extension/ContentView.swift
+++ Counter WatchKit Extension/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var counter = 0
+    @State private var showReset = false
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        NavigationView {
+            ZStack{
+                Button("\(counter)"){
+                    counter += 1
+                    withAnimation{
+                    showReset = (counter > 0) ? true : false
+                    }
+                }
+                .font(.largeTitle)
+                .foregroundStyle(.black)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(.yellow))
+                .clipShape(Circle())
+                
+                if showReset {
+                        VStack{
+                            HStack{
+                                Button("⤴", action: reset)
+                                    .frame(width: 50, height: 50)
+                                    .background(Capsule().fill(Color.red))
+                                    .clipShape(Circle())
+                                Spacer()
+                            }
+                        Spacer()
+                        }
+                        .transition(.asymmetric(insertion: .opacity, removal: .slide))
+                }
+            }
+        }
+    }
+    func reset(){
+        counter = 0
+        withAnimation {
+        showReset = false
+        }
     }
 }
 
@@ -19,3 +56,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
